35a617e627fdd557b1942280470c6af7870b70da
Graham <g_vickers@fanshaweonline.ca>
2019-10-07 23:04:31 -0400

    ive linked the database with js, but getting "TypeError: Cannot read property render of undefined" this needs to be fixed

--- app.js
+++ app.js
@@ -7,16 +7,32 @@ const sql = require('./utils/sql.js')
 const port = process.env.PORT || 3000; 
 const app = express();
 
+// defining the varriable that will be used in order to run the query
+var sqlArrHead = [],
+    sqlArrParag = [],
+    sqlArrImg = [];
+
+
+
 app.use(express.static('public'));
 
 // tell exprss to use the handlebars engine to render data
 app.set('view engine', 'hbs');
 
 // tell express to use tghe views folder to find this template
-app.set('views', __dirname + '/views');
+// app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname + "/views"));
+
+app.use(express.static(path.join('public')));
+
+app.get(`/`, (req, res) =>  {
+  console.log('youre on the home page');
+  res.render('home');
+})
 
 
-app.get('/', (req, res) => {
+// this is the query that will handle bringing the data from the table "tb_home" and populate into the home.hbs
+app.get;('/', (req, res) => {
 
   sql.getConnection((err, connection) => {
       if(err) {
@@ -33,26 +49,26 @@ app.get('/', (req, res) => {
               sqlArrHead.push(row.Heading);
               sqlArrParag.push(row.Paragraph);
           })
-          
+            // defining all handlebar names and where the data is from
             res.render('home', {
-                contentOneImg: sqlArrText[0],
-                contentOneHead: sqlArrName[0],
-                contentOneParag: sqlArrRate[0], 
-                contentTwoImg: sqlArrText[1],
-                contentTwoHead: sqlArrName[1],
-                contentTwoParag: sqlArrRate[1],
+                contentOneImg: sqlArrImg[0],
+                contentOneHead: sqlArrHead[0],
+                contentOneParag: sqlArrParag[0], 
+                contentTwoImg: sqlArrImg[1],
+                contentTwoHead: sqlArrHead[1],
+                contentTwoParag: sqlArrParag[1],
                 contentThreeImg: sqlArrText[2],
-                contentThreeHead: sqlArrName[2],
-                contentThreeParag: sqlArrRate[2], 
-                contentFourImg: sqlArrText[3],
-                contentFourHead: sqlArrName[3],
-                contentFourParag: sqlArrRate[3],
-                contentFiveImg: sqlArrText[4],
-                contentFiveHead: sqlArrName[4],
-                contentFiveParag: sqlArrRate[4],
-                contentSixImg: sqlArrText[5],
+                contentThreeHead: sqlArrHead[2],
+                contentThreeParag: sqlArrParag[2], 
+                contentFourImg: sqlArrImg[3],
+                contentFourHead: sqlArrHead[3],
+                contentFourParag: sqlArrParag[3],
+                contentFiveImg: sqlArrImg[4],
+                contentFiveHead: sqlArrHead[4],
+                contentFiveParag: sqlArrParag[4],
+                contentSixImg: sqlArrImg[5],
                 contentSixHead: sqlArrName[5],
-                contentSixParag: sqlArrRate[5]
+                contentSixParag: sqlArrParag[5]
             });
 
             console.log(sqlArrImg);
--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
     upass: 'root',
     port: 8889, 
     host: 'localhost', 
-    db: 'hackathon'
+    db: 'db_hackathon'
 }
 
 
